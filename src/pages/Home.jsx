9a82dda6fd87ef9f7b3f411a7a26706f8e368635
muhammaddariazzidane <dariazzidane@gmail.com>
2023-05-09 16:02:37 +0700

    test

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,7 +12,7 @@ export default function Home() {
             <div class="max-w-lg mx-auto">
               <h1 class="text-3xl font-semibold text-gray-800 dark:text-white lg:text-4xl">Muhammad Dariaz Zidane</h1>
               <p class="mt-6 text-gray-500 dark:text-gray-300">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero similique obcaecati illum mollitia.</p>
-              <button class="px-5 py-2 mt-6 text-sm font-medium leading-5 text-center text-white capitalize bg-blue-600 rounded-lg hover:bg-blue-500 lg:mx-0 lg:w-auto focus:outline-none">Start 14-Day free trial</button>
+              <button class="px-5 py-2 mt-6 text-sm font-medium leading-5 text-center text-white capitalize bg-blue-600 rounded-lg hover:bg-blue-500 lg:mx-0 lg:w-auto focus:outline-none">Download CV</button>
             </div>
 
             <div class="flex relative justify-center mt-10">
